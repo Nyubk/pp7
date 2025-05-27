33978d62217fd2af2870c196898a0e19d306a34a
morty777 <vc.rincon31@gmail.com>
2025-05-27 05:20:44 -0600

    solución a falla matploit

--- simulador_dashboard.py
+++ simulador_dashboard.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+import matplotlib
+from matplotlib import matplotlib.pyplot as plt
 import numpy as np
 import sklearn
 
